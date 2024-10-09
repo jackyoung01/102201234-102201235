1fdeefb838c65d23186e959d7009ec3ad72e5314
jackyoung01 <154318863+jackyoung01@users.noreply.github.com>
2024-10-09 21:44:11 +0800

    Add files via upload

---
+++ inc/config.php
@@ -0,0 +1,12 @@
+<?php
+
+
+
+	$mysql_server_name='localhost'; //Mysql数据库服务器
+	
+	$mysql_username='ser9257516814'; //Mysql数据库用户名
+	
+	$mysql_password='5wmoGA'; //Mysql数据库密码
+	
+	$mysql_database='ser9257516814'; //Mysql数据库名
+?>
